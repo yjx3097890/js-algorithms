673b6c88885e7d20940796046cb1c365dfea1398
yanjixian <315644588@qq.com>
2024-02-28 01:36:26 +0800

    数组和链表

---
+++ array/selectSort.js
@@ -0,0 +1,5 @@
+const arr = [4,1,6,9,3,2,8,7];
+
+function selectSort(arr) {
+
+}
---
+++ linkedList/reverse.js
@@ -0,0 +1,23 @@
+class Node {
+    value=null;
+    next=null;
+
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+const node1 = new Node(1);
+const node2 = new Node(2);
+const node3 = new Node(3);
+const node4 = new Node(4);
+const node5 = new Node(5);
+
+node1.next = node2;
+node2.next = node3;
+node3.next = node4;
+node4.next = node5;
+
+function reverse(head) {
+
+}
