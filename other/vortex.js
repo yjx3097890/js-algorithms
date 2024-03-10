4490f9885065b2ea475a8c05ee9aea9af733810a
yanjixian <315644588@qq.com>
2024-03-10 15:04:05 +0800

    漩涡二维数组答案

--- other/vortex.js
+++ other/vortex.js
@@ -4,7 +4,38 @@
  * @param n 列数
  */
 function vortex(m, n){
-
+   const result = new Array(m).fill(0).map(() => Array(n).fill(0));
+    ring(result, m, n, 1, 1, 1);
+   return result;
 }
 
-console.log(vortex(3,4))
+/**
+ *
+ * @param result
+ * @param m
+ * @param n
+ * @param i 从1开始
+ * @param j 从1开始
+ * @param value
+ * @return {*}
+ */
+function ring(result, m, n, i, j, value){
+    if (i > m-i + 1  || j> n-j+1) return result;
+    const total = m*n;
+    let num = value;
+    for (let k = j-1; k < n - j + 1&& num <= total; k++) {
+        result[i-1][k] = num++;
+    }
+    for (let k = i; k < m - i +1&& num <= total; k++) {
+        result[k][n-j] = num++;
+    }
+    for (let k = n-j-1; k >= j-1 && num <= total; k--) {
+        result[m-i][k] = num++;
+    }
+    for (let k = m-i-1; k >= i && num <= total; k--) {
+        result[k][j-1] = num++;
+    }
+    return ring(result, m, n, i+1, j+1, num);
+}
+
+console.log(vortex(5,4))
