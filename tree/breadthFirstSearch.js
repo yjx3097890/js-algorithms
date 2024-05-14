c0b2b6eb7528dc45ea64ab42b6dd3eb8d376eb38
yanjixian <315644588@qq.com>
2024-05-15 01:29:00 +0800

    搜索二叉树

---
+++ tree/breadthFirstSearch.js
@@ -0,0 +1,29 @@
+class Node {
+    value = null;
+    left = null;
+    right = null;
+
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+const f = new Node('F');
+const g = new Node('G');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.left = f;
+c.right = g;
+
+function breadthFirstSearch(root, target) {
+}
+
+console.log(breadthFirstSearch(a, 'D')); // true
