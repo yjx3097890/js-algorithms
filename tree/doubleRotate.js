a15815bdffcb97cf385f8212d5a0dcb61e5e084b
阎继先 <315644588@qq.com>
2024-05-16 15:09:00 +0800

    平衡二叉树双旋

--- tree/doubleRotate.js
+++ tree/doubleRotate.js
@@ -23,9 +23,51 @@ b.left = c;
 b.right = d;
 d.left = e;
 
+function getDeep(root) {
+    if (root == null) return 0;
+    const leftDeep = getDeep(root.left);
+    const rightDeep = getDeep(root.right);
+    return Math.max(leftDeep, rightDeep) + 1;
+}
+
+// 左旋
+function leftRotate(root) {
+    const newRoot = root.right;
+    root.right = root.right.left;
+    newRoot.left = root;
+    return newRoot;
+}
+
+// 右旋
+function rightRotate(root) {
+    const newRoot = root.left;
+    root.left = root.left.right;
+    newRoot.right = root;
+    return newRoot;
+}
 
 function toBalance(root) {
-
+    if (isBalancedBinaryTree(root)) return root;
+    root.left = toBalance(root.left);
+    root.right = toBalance(root.right);
+    const leftDeep = getDeep(root.left);
+    const rightDeep = getDeep(root.right);
+    if (leftDeep - rightDeep > 1) {
+        const rightDeep = getDeep(root.left.right);
+        const leftDeep = getDeep(root.left.left);
+        if (rightDeep > leftDeep) {
+            root.left = leftRotate(root.left);
+        }
+        return rightRotate(root);
+    } else if (rightDeep - leftDeep > 1) {
+        const rightDeep = getDeep(root.right.right);
+        const leftDeep = getDeep(root.right.left);
+        if (leftDeep > rightDeep) {
+            root.right = rightRotate(root.right);
+        }
+        return leftRotate(root);
+    }
+    return root;
 }
 
 console.log(isBalancedBinaryTree(a));
@@ -33,4 +75,3 @@ console.log(isBalancedBinaryTree(a));
 const newRoot = toBalance(a);
 
 console.log(isBalancedBinaryTree(newRoot));
-console.log(newRoot);
