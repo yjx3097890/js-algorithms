502d33fb396a8ab35ae352ef7369bd484987f795
yanjixian <315644588@qq.com>
2024-05-19 00:48:20 +0800

    图的深搜，广搜

---
+++ graph/breadthSearch.js
@@ -0,0 +1,30 @@
+class Node {
+    value = null;
+    neighbors = [];
+
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+// 创建节点
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+
+
+// 构建邻接关系
+nodeA.neighbors.push(nodeB, nodeC, nodeD);
+nodeB.neighbors.push(nodeA, nodeC);
+nodeC.neighbors.push(nodeA, nodeB, nodeD);
+nodeD.neighbors.push(nodeA, nodeC, nodeE);
+nodeE.neighbors.push(nodeD);
+
+
+function breadthSearch(rootList, target, path = []) {
+
+}
+
+console.log(breadthSearch([nodeA], 'E'))
