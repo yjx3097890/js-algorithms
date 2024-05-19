7d59fbc7b6e15c4c0353903a45c9b42487c04896
yanjixian <315644588@qq.com>
2024-05-20 01:18:28 +0800

    普利姆算法和克鲁斯卡尔算法

---
+++ graph/kruskal.js
@@ -0,0 +1,44 @@
+class Node {
+    value = null;
+    neighbors = [];
+
+    constructor(value) {
+        this.value = value;
+    }
+
+    toString() {
+        return this.value + ' -> ' + this.neighbors.map(node => node.value).join(', ');
+    }
+
+}
+
+// 创建节点
+const nodeA = new Node('A');
+const nodeB = new Node('B');
+const nodeC = new Node('C');
+const nodeD = new Node('D');
+const nodeE = new Node('E');
+
+const nodes = [nodeA, nodeB, nodeC, nodeD, nodeE];
+
+const graphMatrix = [
+    [0, 4, 8, 6, Infinity],
+    [4, 0, 7, Infinity, Infinity],
+    [8, 7, 0, 5, Infinity],
+    [6, Infinity, 5, 0, 7],
+    [Infinity, Infinity, Infinity, 7, 0]
+];
+
+// kruskal算法
+function kruskal(nodes, graphMatrix) {
+
+}
+
+kruskal(nodes, graphMatrix);
+console.log(nodeA.toString());
+console.log(nodeB.toString());
+console.log(nodeC.toString());
+console.log(nodeD.toString());
+console.log(nodeE.toString());
+
+
