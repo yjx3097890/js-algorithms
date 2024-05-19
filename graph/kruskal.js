bff1d1bd09f76259a86867ddfe52e0b499ddc605
yanjixian <315644588@qq.com>
2024-05-20 01:19:32 +0800

    普利姆算法和克鲁斯卡尔算法

--- graph/kruskal.js
+++ graph/kruskal.js
@@ -29,9 +29,41 @@ const graphMatrix = [
     [Infinity, Infinity, Infinity, 7, 0]
 ];
 
+function isConnect(nodeA, nodeB, visited = new Set()) {
+    if (nodeA.neighbors.includes(nodeB) || nodeB.neighbors.includes(nodeA)) {
+        return true;
+    }
+    for (const neighbor of nodeA.neighbors) {
+        if (visited.has(neighbor)) {
+            continue;
+        }
+        visited.add(neighbor);
+        if (isConnect(neighbor, nodeB, visited)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 // kruskal算法
 function kruskal(nodes, graphMatrix) {
-
+    let begin = null;
+    let end = null;
+    let minWeight = Infinity;
+    for (let i = 0; i < graphMatrix.length; i++) {
+        for (let j = i + 1; j < graphMatrix[i].length; j++) {
+            if (i !== j && graphMatrix[i][j] < minWeight && !isConnect(nodes[i], nodes[j])) {
+                minWeight = graphMatrix[i][j];
+                begin = nodes[i];
+                end = nodes[j];
+            }
+        }
+    }
+    if (begin && end) {
+        begin.neighbors.push(end);
+        end.neighbors.push(begin);
+        kruskal(nodes, graphMatrix);
+    }
 }
 
 kruskal(nodes, graphMatrix);
@@ -40,5 +72,3 @@ console.log(nodeB.toString());
 console.log(nodeC.toString());
 console.log(nodeD.toString());
 console.log(nodeE.toString());
-
-
--- graph/prim.js
+++ graph/prim.js
@@ -31,7 +31,38 @@ const graphMatrix = [
 
 // 普利姆算法
 function prim(nodes, graphMatrix, addedList = [], restList = [...nodes]) {
-
+    if (restList.length === 0) {
+        return;
+    }
+    if (addedList.length === 0) {
+        addedList.push(restList.shift());
+        prim(nodes, graphMatrix, addedList, restList);
+        return;
+    }
+    let minWeight = Infinity;
+    let minIndex = -1;
+    let addedIndex = -1;
+    for (const restNode of restList) {
+        const restIndex = nodes.indexOf(restNode);
+        for (const addedNode of addedList) {
+            const nodeIndex = nodes.indexOf(addedNode);
+            const weight = graphMatrix[restIndex][nodeIndex];
+            if (weight < minWeight) {
+                minWeight = weight;
+                minIndex = restIndex;
+                addedIndex = nodeIndex;
+            }
+        }
+    }
+    if (minIndex !== -1) {
+        const minNode = nodes[minIndex];
+        const node = nodes[addedIndex];
+        node.neighbors.push(minNode);
+        minNode.neighbors.push(node);
+        addedList.push(minNode);
+        restList = restList.filter(node => node !== minNode);
+    }
+    prim(nodes, graphMatrix, addedList, restList);
 }
 
 prim(nodes, graphMatrix);
@@ -40,5 +71,3 @@ console.log(nodeB.toString());
 console.log(nodeC.toString());
 console.log(nodeD.toString());
 console.log(nodeE.toString());
-
-
