2bd6dc108deea35d5f5439e66d70b3edc4048ed5
yanjixian <315644588@qq.com>
2024-05-20 02:01:01 +0800

    动态规划之青蛙跳台阶

--- dynamicProgramming/fogJump.js
+++ dynamicProgramming/fogJump.js
@@ -1,6 +1,11 @@
 // 一个青蛙，一次只能跳一级台阶，或者跳两级台阶。
 // 问：这个青蛙跳上 n 级台阶有多少种跳法。
 
-function fogJump(n) {
+// f(n) = f(n-1) + f(n-2)
 
+function fogJump(n) {
+    if (n <= 0) return -1;
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    return fogJump(n-1) + fogJump(n-2);
 }
--- dynamicProgramming/fogJumpHard.js
+++ dynamicProgramming/fogJumpHard.js
@@ -1,6 +1,16 @@
 // 这只青蛙，一次可以跳1级台阶、2级台阶、或n级台阶。
 // 问：这只青蛙，跳上n级台阶有多少种方法?
 
-function fogJumpHard(n) {
+// f(n) = f(n-1) + f(n-2) + f(n-3) + …… + f(2) + f(1) + 1
 
+function fogJumpHard(n) {
+    if (n <= 0) return -1;
+    if (n === 1) return 1;
+    let result = 0;
+    for (let i = 1 ; i < n ; i ++) {
+        result += fogJumpHard(n - i);
+    }
+    return result + 1;
 }
+
+console.log(fogJumpHard(8));
