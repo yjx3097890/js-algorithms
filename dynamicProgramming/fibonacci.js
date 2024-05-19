fab8b27be7a23dd0c77d2d5078bf584c5c0f7191
yanjixian <315644588@qq.com>
2024-05-20 01:37:25 +0800

    动态规划之斐波那契数列

--- dynamicProgramming/fibonacci.js
+++ dynamicProgramming/fibonacci.js
@@ -2,8 +2,24 @@
 
 // 循环的方式
 function fibonacci1(n) {
+    if (n < 0) return -1;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    let a = 0;
+    let b = 1;
+    let result = 0;
+    for (let i = 2 ; i <= n ; i ++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
 }
 
 // 递归的方式
 function fibonacci(n) {
+    if (n < 0) return -1;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
